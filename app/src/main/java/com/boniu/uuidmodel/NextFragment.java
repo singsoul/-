431ce9b289694c417ca60ff768fc9dba325e6057
胡永辉 <824300438@qq.com>
2020-06-03 11:48:02 +0800

    1

--- app/src/main/java/com/boniu/uuidmodel/NextFragment.java
+++ app/src/main/java/com/boniu/uuidmodel/NextFragment.java
@@ -1,11 +1,16 @@
 package com.boniu.uuidmodel;
 
-import com.bumptech.glide.Glide;
+
+import android.util.Log;
+
+import com.boniu.uuidsdk.UuidCreator;
 
 public class NextFragment extends MyFragment {
 
 
     @Override
     public void onclick() {
+        String deviceId = UuidCreator.getInstance(getActivity()).getDeviceId();
+        Log.e("asd", "onclick: " + deviceId );
     }
 }
